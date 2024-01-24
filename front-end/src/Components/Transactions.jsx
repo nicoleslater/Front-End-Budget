ea4680ab1dbf9f9481ce3c592332e5fcd295b297
Nicole Slater <nicoleslater@pursuit.org>
2024-01-24 14:35:44 -0500

    Updating TransactionNewForm.jsx

---
+++ front-end/src/Components/Transactions.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Transaction from './Transaction';
+
+const API = import.meta.env.VITE_BASE_URL;
+
+function Transactions() {
+  const [ transactions, setTransactions ] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/transactions`)
+      .then(response => response.json())
+      .then((responseJSON) => {
+        console.log(responseJSON);
+        setTransactions(responseJSON.data.payload);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="Transactions">
+        <section>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                    
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map((transaction) => {
+                        return <Transaction key={transaction.id} transaction={transaction} />;
+                    })}
+                </tbody>
+            </table>
+        </section>
+    </div>
+);
+}
+export default Transactions;
