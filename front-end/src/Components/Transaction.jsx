7dd738f706cb394b99b51440db54e1b3632f60d2
Nicole Slater <nicoleslater@pursuit.org>
2024-01-24 14:17:53 -0500

    Updated Transaction Details. jsx

--- front-end/src/Components/Transaction.jsx
+++ front-end/src/Components/Transaction.jsx
@@ -1,29 +1,21 @@
-import { Link } from "react-router-dom";
-
 function Transaction({ transaction }){
-    return (
-        <tr>
-            <td>
-                {transaction.is_priority ? (
-                    <span>💰</span>
-                ) : (
-                    <span>&nbsp; &nbsp; &nbsp;</span>
-                )}
-            </td>
-            <td>
-                {transaction.item_name}
-            </td>
-            <td>
-                {transaction.amount}
-            </td>
-            <td>
-                {transaction.due_date}
-            </td>
-            <td>
-                <Link to={`/transactions/${transaction.id}`}>🏦</Link>
-            </td>
 
-        </tr>
-    );
-}
+
+ 
+    return(
+      <div>
+        <section>
+            <h5>{transaction.name}</h5>
+           
+            <p>{transaction.amount}</p>
+            <p>{transaction.deadline}</p>
+            <p>{transaction.priority}</p>
+    
+            </section>
+           </div>
+    )
+  }
+
+
+
 export default Transaction;
--- front-end/src/Components/TransactionDetails.jsx
+++ front-end/src/Components/TransactionDetails.jsx
@@ -4,44 +4,60 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 const API = import.meta.env.VITE_BASE_URL;
 
 function TransactionDetails(){
-    const [ transaction, setTransaction ] = useState([]);
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const [ transaction, setTransaction ] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch(`${API}/transactions/${id}`)
-        .then((response) => response.json())
-        .then((responseJSON) => {
-            setTransaction(responseJSON)
-        })
-        .catch(error => console.log(error))
-    }, [id, API])
+  useEffect(() => {
+    fetch(`${API}/transactions/${id}`)
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      setTransaction(responseJSON)
+    })
+    .catch(error => console.log(error))
+  }, [id, API])
 
-    const deleteTransaction = () => {
-        const httpOptions = { method: "DELETE" };
-        fetch(`${API}/transactions/${id}`, httpOptions)
-          .then(() => navigate(`/transactions`))
-          .catch((error) => console.log(error));
-      };
-    
-      const handleDelete = () => {
-        deleteTransaction();
-      };
+  const deleteTransaction = () => {
+    const httpOptions = { method: "DELETE" };
+    fetch(`${API}/transactions/${id}`, httpOptions)
+      .then(() => navigate(`/transactions`))
+      .catch((error) => console.log(error));
+  };
 
-      return (
+  const handleDelete = () => {
+    deleteTransaction();
+  };
+
+  return (
+
+<div> 
+    <article> 
+   
+        <h5>
+            <span>
+              
+              
+            </span>
+            &nbsp;&nbsp;&nbsp;&nbsp; 
+        </h5>
+        <h4>{transaction.name}</h4>
+        <h4>{transaction.deadline}</h4>
+        <h4>{transaction.priority}</h4>
+        <div className="navigation"> 
         <div>
-            <article>
-              <h5>
-                <span>
-                <a href={transaction.name}></a>
-                </span>
-                <h4>
-                  {transaction.date}
-                </h4>
-              </h5>
-            </article>
         </div>
-      )
+        <div>
+            <Link to={`/transactions/new`}>
+            <button>Add Budget Transaction</button>
+            </Link>
+        </div>
+            <button onClick={handleDelete}>Delete Transaction</button>
+        </div>
+    </article>
+</div>
+
+);
+
 }
 
 export default TransactionDetails;
