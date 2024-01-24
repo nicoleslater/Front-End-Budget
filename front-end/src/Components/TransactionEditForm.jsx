d1a40bf2c5c2d64ecf4747ecae63e60dbcef5803
Nicole Slater <nicoleslater@pursuit.org>
2024-01-24 14:26:50 -0500

    Updating Transaction Edit Form.jsx

---
+++ front-end/src/Components/TransactionEditForm.jsx
@@ -0,0 +1,133 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+
+
+const API = import.meta.env.VITE_BASE_URL;
+
+function TransactionEditForm(){
+    let { id } = useParams();
+    const navigate = useNavigate();
+
+    const [ transaction, setTransaction ] = useState({
+        name: "",
+        amount: 0,
+        deadline: "",
+        priority: false
+    });
+
+    const handleTextChange = (event) => {
+        setTransaction({ ...product, [event.target.id]: event.target.value });
+    };
+
+    const handleCheckboxChange = () => {
+        setTransaction({ ...transaction, priority: !transaction.priority });
+    };
+
+    const updateTransaction = () => {
+            console.log(`${API}/transactions/${id}`)
+
+            fetch(`${API}/transactions/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(transaction),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => {
+            navigate(`/transactions/${id}`);
+        })
+        .catch((error) => console.error("catch", error));
+    };
+
+    useEffect(() => {
+        fetch(`${API}/transactions/${id}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((responseJSON) => {
+            setTransaction(responseJSON);
+        })
+        .catch((error) => console.error(error));
+    }, [id]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        updateTransaction();
+    };
+
+    return (
+        <div className="Edit">
+            <h2>Add Transaction to Budget</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label htmlFor="name">Name:</label>
+                </td>
+                <td>
+                  <input
+                    id="name"
+                    value={transaction.name}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Enter Name..."
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label htmlFor="Deadline">Deadline:</label>
+                </td>
+                <td>
+                  <input
+                    id="deadline"
+                    value={transaction.deadline}
+                    type="text"
+                    onChange={handleTextChange}
+                    placeholder="Enter deadline"
+                    required
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label htmlFor="priority">Priority?</label>
+                </td>
+                <td>
+                  <input
+                    id="priority"
+                    value={transaction.priority}
+                    type="checkbox"
+                    onChange={handleCheckboxChange}
+                    checked={transaction.priority}
+                    required
+                  />
+                </td>
+              </tr>
+              
+              <div>
+                  <label htmlFor="amount">Amount:$</label>
+                  <input
+                    id="amount"
+                    value={transaction.amount}
+                    type="text"
+                    required
+                  />
+              </div>
+            </tbody>
+          </table>
+          <div>
+            <button type="submit" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+      
+          <Link to={`/transactions/${id}`}>
+            <button>Changed My Mind! 😌 </button>
+          </Link>
+        </div>
+      );
+    }
+    
+    export default TransactionEditForm;
