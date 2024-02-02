6b3e73f3a7072b1f3b346cfc5195d803bf498b5a
Nicole Slater <nicoleslater@pursuit.org>
2024-02-02 17:05:05 -0500

    Updating Transaction.jsx

--- front-end/src/Components/Transaction.jsx
+++ front-end/src/Components/Transaction.jsx
@@ -13,13 +13,15 @@ function Transaction({transaction}){
     <div class="transactionCardList-card-grid">
     <div class="booster">
       <div class="cardTitle">
-        <h1>{transaction.name}</h1>
+        <h3>{transaction.name}</h3>
+        <p>{transaction.amount}</p>
       </div>
-      <div class="imageURL">
+      {/* <div class="imageURL">
         <img src="https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=386549&type=card"></img>
-      </div>
+      </div> */}
       <div class="cardText">
-        <p>lorem ipsum blah</p>
+        <p>{transaction.deadline}</p>
+        <p>{transaction.priority}</p>
       </div>
     </div>
            </div>
