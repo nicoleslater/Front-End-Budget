65759debea99f560f4d0ad331dc0946e1811e98f
Nicole Slater <nicoleslater@pursuit.org>
2024-02-02 17:18:02 -0500

    Updated CSS for Transaction Card

--- front-end/src/Components/Transaction.jsx
+++ front-end/src/Components/Transaction.jsx
@@ -16,9 +16,9 @@ function Transaction({transaction}){
         <h3>{transaction.name}</h3>
         <p>{transaction.amount}</p>
       </div>
-      {/* <div class="imageURL">
+      <div class="imageURL">
         <img src="https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=386549&type=card"></img>
-      </div> */}
+      </div>
       <div class="cardText">
         <p>{transaction.deadline}</p>
         <p>{transaction.priority}</p>
