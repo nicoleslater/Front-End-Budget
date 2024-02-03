4ccf012d7e8254a14b3a819ea5d0e6328fbbcca5
Nicole Slater <nicoleslater@pursuit.org>
2024-02-02 21:01:26 -0500

    Updated CSS for Transaction

--- front-end/src/Components/Transaction.jsx
+++ front-end/src/Components/Transaction.jsx
@@ -11,21 +11,36 @@ function Transaction({transaction}){
 return(
     
       
-      <div className="transactionCardList-card-grid">
-        <div className="booster">
-          <div className="cardTitle">
-            <h3>{transaction.name}</h3>
-            <p>${transaction.amount}</p>
-          </div>
-          <div className="image">
-            <img src="https://images.unsplash.com/photo-1531482615713-2afd69097998?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGZpbmFuY2V8ZW58MHx8MHx8fDA%3D" alt="Transaction" />
-          </div>
-          <div className="cardText">
-            <p>Deadline: {transaction.deadline}</p>
-            <p>Priority: {transaction.priority ? 'High' : 'Low'}</p>
-          </div>
-        </div>
+  <div className="transactionCardList-card-grid">
+  <div className="booster">
+    <div className="cardTitle">
+      <h3>{transaction.name}</h3>
+      <p>${transaction.amount}</p>
     </div>
+    <div className="image">
+      <img src="https://images.unsplash.com/photo-1531482615713-2afd69097998?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGZpbmFuY2V8ZW58MHx8MHx8fDA%3D" alt="Transaction" />
+    </div>
+    <div className="cardText">
+      <div className="property">
+        <span>Vendor:</span>
+        <span>{transaction.vendor}</span>
+      </div>
+      <div className="property">
+        <span>Category:</span>
+        <span>{transaction.category}</span>
+      </div>
+      <div className="property">
+        <span>Deadline:</span>
+        <span>{transaction.deadline}</span>
+      </div>
+      <div className="property">
+        <span>Priority:</span>
+        <span>{transaction.priority ? 'High' : 'Low'}</span>
+      </div>
+    </div>
+  </div>
+</div>
+
     
     )
   }
